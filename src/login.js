03f66142a583a849515318c60f5b314566422131
stephenlambertfs <97977357+stephenlambertfs@users.noreply.github.com>
2022-03-15 14:50:18 -0600

    Removing solution

--- src/login.js
+++ src/login.js
@@ -3,18 +3,6 @@ import React from 'react'
 function Login(props){
     return(
     <>
-    <h3 style={{color: "whitesmoke",height: "",position:"center"}}>{props.title}</h3>
-    <form>
-        <label>{props.users}
-            <input type="text" name="name" />
-        </label>
-        <input type="submit" value="Submit" />
-        <br></br>
-        <label>{props.pwd}
-            <input type="text" name="pwd" />
-        </label>
-        <input type="submit" value="Submit" />
-    </form>
     </>
     )
 }
